c2277022aa40cbc17db566b6e0c93d3119c23863
hayate112 <k.hayate0112@gmail.com>
2021-03-15 02:12:26 +0000

    review update

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -1,8 +1,10 @@
 class Users::ReviewsController < ApplicationController
   def create
-    @review = Review.new(review_params)
-    if @review.save
-      redirect_to root_path
+    item = Item.find(params[:item_id])
+    review = current_user.reviews.new(review_params)
+    review.item_id = item.id
+    if review.save
+      redirect_to item_path(item.id)
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :itme
+  belongs_to :item
 end
