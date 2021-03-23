db071f5ffa759ffe5072a64bcdfd776cf3b2f7c1
hayate112 <k.hayate0112@gmail.com>
2021-03-23 03:31:27 +0000

    remote

--- app/controllers/users/blog_likes_controller.rb
+++ app/controllers/users/blog_likes_controller.rb
@@ -1,15 +1,13 @@
 class Users::BlogLikesController < ApplicationController
   def create
-    blog = Blog.find(params[:blog_id])
-    blog_like = current_user.blog_likes.new(blog_id: blog.id)
+    @blog = Blog.find(params[:blog_id])
+    blog_like = current_user.blog_likes.new(blog_id: @blog.id)
     blog_like.save
-    redirect_to blog_path(blog)
   end
 
   def destroy
-    blog = Blog.find(params[:blog_id])
-    blog_like = current_user.blog_likes.find_by(blog_id: blog.id)
+    @blog = Blog.find(params[:blog_id])
+    blog_like = current_user.blog_likes.find_by(blog_id: @blog.id)
     blog_like.destroy
-    redirect_to blog_path(blog)
   end
 end
